df684809e6c78ccb0ec32832ddd06123a611f918
Shahbaz <shahbaz6295@gmail.com>
2024-09-03 01:45:54 +0530

    version-4

---
+++ backend/frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  server:{
+
+    proxy:{
+     "/api/":"http://localhost:5000",
+     "/uploads/":"http://localhost:5000",
+     "/payUploads/":"http://localhost:5000"
+    },
+  },
+})
