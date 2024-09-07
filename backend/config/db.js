7bbfe2b94e9dae456888faa5e68af9ed5c491109
Shahbaz <shahbaz6295@gmail.com>
2024-09-07 17:31:24 +0530

    version-19

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,9 @@
 import mongoose from "mongoose";
-import { MongoClient, ServerApiVersion } from 'mongodb';
+
 const connectDB=async ()=>{
 
-
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGO_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-
-
   try {
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Successfully connected to MongoDB👍")
   } catch (error) {
     console.log(`ERROR: ${error.message}`)
